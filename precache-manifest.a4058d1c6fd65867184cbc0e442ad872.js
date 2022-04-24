681cfaf95a210a7fb366bc9b2cedd93e9e9704d8
Maromaru <w1193@likelion.org>
2022-04-24 22:15:14 +0900

    Updates

---
+++ precache-manifest.a4058d1c6fd65867184cbc0e442ad872.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "780fa966d98d682847a8",
+    "url": "/Perdaba/static/css/main.9c1f7a16.chunk.css"
+  },
+  {
+    "revision": "780fa966d98d682847a8",
+    "url": "/Perdaba/static/js/main.780fa966.chunk.js"
+  },
+  {
+    "revision": "580681ed314b03671075",
+    "url": "/Perdaba/static/js/runtime~main.580681ed.js"
+  },
+  {
+    "revision": "3dfe56a0a60e2401b1d6",
+    "url": "/Perdaba/static/js/2.3dfe56a0.chunk.js"
+  },
+  {
+    "revision": "c3d9cbf4fd9f75271d0bcef9e35c1a34",
+    "url": "/Perdaba/index.html"
+  }
+];
